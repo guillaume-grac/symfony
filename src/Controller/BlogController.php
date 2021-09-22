48b9e9b756ea9ade382b02e054cf76b27dff30c3
Guillaume <gui.grc@yahoo.com>
2021-09-22 15:54:10 +0200

    Inscription connexion

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Config\Framework\RequestConfig;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use function Sodium\add;
 
 
@@ -51,6 +53,10 @@ class BlogController extends AbstractController
 
         $form = $this->createFormBuilder($article)
             ->add('titre')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'titre'
+            ])
             ->add('contenu')
             ->add('image')
             ->getForm();
